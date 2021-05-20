e04f4bc3a1b96c60ee9bfcbf4f2dd9d467aca866
thekoneko <67551503+thekoneko@users.noreply.github.com>
2021-05-20 20:12:46 +0200

    test

--- public/js/info.js
+++ public/js/info.js
@@ -4,5 +4,8 @@ function gotoRandom() {
     if (localStorage.page != "Info") {} else {
         console.log('Info page active.')
         // document.body.appendChild(document.createElement('h1').innerText = "hello")
+        let p = document.createElement('p')
+        p.textContent = 'Work in progress!'
+        main.appendChild(p)
     }
 }
